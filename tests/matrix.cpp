430dc957a93c2bad76de499ebdc02c3d59e78057
Link-God <32190920+Link-God@users.noreply.github.com>
2018-03-21 20:08:39 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE("creating matrix")
 {
-    matrix_t matrix;
+    matrix_t<int> matrix;
     REQUIRE( matrix.rows() == 0 );
     REQUIRE( matrix.collumns() == 0 );
 }
@@ -17,7 +17,7 @@ TEST_CASE("reading matrix")
         "1 1 1\n"
         "2 2 2\n"
         "3 3 3" };
-    matrix_t matrix;
+    matrix_t<int> matrix;
     std::istringstream istream{ input };
     
     REQUIRE( matrix.read( istream ) );
@@ -47,7 +47,40 @@ TEST_CASE("Matrix Add")
         "3 3 3\n"
         "3 3 3\n"
         "3 3 3" };
-    matrix_t matrix1 , matrix2 ,result;
+    matrix_t<int> matrix1 ; matrix_t<int> matrix2; matrix_t<int> result;
+    
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    
+    matrix1.read( istream1 ) ;
+    matrix2.read( istream2 ) ;
+    
+    result= matrix1 + matrix2 ;
+        
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( output == ostream.str() );
+}
+
+TEST_CASE("Matrix Add 2")
+{
+    std::string input1{
+        "3, 3\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5" };
+     std::string input2{
+        "3, 3\n"
+        "2.6 2.6 2.6\n"
+        "2.6 2.6 2.6\n"
+        "2.6 2.6 2.6" };
+     std::string output{
+        "3, 3\n"
+        "4.1 4.1 4.1\n"
+        "4.1 4.1 4.1\n"
+        "4.1 4.1 4.1" };
+    matrix_t<float> matrix1 ;  matrix_t<float> matrix2;  matrix_t<float> result;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -80,7 +113,7 @@ TEST_CASE("Matrix Sub")
         "4 4 4\n"
         "4 4 4\n"
         "4 4 4" };
-    matrix_t matrix1 , matrix2 ,result;
+    matrix_t<int> matrix1 ;matrix_t<int> matrix2 ;matrix_t<int> result;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -96,6 +129,38 @@ TEST_CASE("Matrix Sub")
     REQUIRE( output == ostream.str() );
 }
 
+TEST_CASE("Matrix Sub 2")
+{
+    std::string input1{
+        "3, 3\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5\n"
+        "1.5 1.5 1.5" };
+     std::string input2{
+        "3, 3\n"
+        "2.6 2.6 2.6\n"
+        "2.6 2.6 2.6\n"
+        "2.6 2.6 2.6" };
+     std::string output{
+        "3, 3\n"
+        "1.1 1.1 1.1\n"
+        "1.1 1.1 1.1\n"
+        "1.1 1.1 1.1" };
+    matrix_t<float> matrix1 ;  matrix_t<float> matrix2;  matrix_t<float> result;
+    
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    
+    matrix1.read( istream1 ) ;
+    matrix2.read( istream2 ) ;
+    
+    result= matrix2 - matrix1 ;
+        
+    std::ostringstream ostream;
+    result.write( ostream );
+    
+    REQUIRE( output == ostream.str() );
+}
 TEST_CASE("Matrix Mul")
 {
     std::string input1{
@@ -113,7 +178,7 @@ TEST_CASE("Matrix Mul")
         "6 6 6\n"
         "6 6 6\n"
         "6 6 6" };
-    matrix_t matrix1 , matrix2 ,result;
+    matrix_t<int> matrix1 ; matrix_t<int> matrix2 ;matrix_t<int> result;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -145,7 +210,8 @@ TEST_CASE("Matrix SelfAdd")
         "3 3 3\n"
         "3 3 3\n"
         "3 3 3" };
-    matrix_t matrix1 , matrix2 ;
+    matrix_t<int> matrix1 ;
+    matrix_t<int> matrix2 ;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -177,7 +243,8 @@ TEST_CASE("Matrix SelfSub")
         "4 4 4\n"
         "4 4 4\n"
         "4 4 4" };
-    matrix_t matrix1 , matrix2;
+    matrix_t<int> matrix1 ;
+    matrix_t<int> matrix2;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -209,7 +276,8 @@ TEST_CASE("Matrix SelfMul")
         "6 6 6\n"
         "6 6 6\n"
         "6 6 6" };
-    matrix_t matrix1 , matrix2 ;
+    matrix_t<int> matrix1 ;
+    matrix_t<int> matrix2 ;
     
     std::istringstream istream1{ input1 };
     std::istringstream istream2{ input2 };
@@ -223,4 +291,55 @@ TEST_CASE("Matrix SelfMul")
     matrix1.write( ostream );
     
     REQUIRE( output == ostream.str() );
+}
+
+TEST_CASE("All is OK")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+     std::string input2{
+        "3, 3\n"
+        "2 2 2\n"
+        "2 2 2\n"
+        "2 2 2" };
+    matrix_t<int> matrix1 ;
+    matrix_t<int> matrix2 ;
+    
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    
+    matrix1.read( istream1 ) ;
+    matrix2.read( istream2 ) ;
+
+    bool good = test (matrix1 , "*=" , matrix2 ) ;
+    bool success = true ;
+    REQUIRE(good == success);
+}
+
+TEST_CASE("All is Not OK")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+     std::string input2{
+        "2, 2\n"
+        "2 2\n"
+        "2 2" };
+    matrix_t<int> matrix1 ;
+    matrix_t<int> matrix2 ;
+    
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    
+    matrix1.read( istream1 ) ;
+    matrix2.read( istream2 ) ;
+    
+    bool bad = test (matrix1 , "+" , matrix2 ) ;
+    bool success = false ;
+    REQUIRE(bad == success);
 }
