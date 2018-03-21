d69b601051dd8dde719b625046d92127eae7189e
Link-God <32190920+Link-God@users.noreply.github.com>
2018-03-21 20:50:25 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -195,7 +195,7 @@ matrix_t<T> & matrix_t<T>::operator *=( matrix_t<T> const & other )
 }
 
 template < typename T > 
-bool test (matrix_t<T> const & mat1 , const char  op  , matrix_t<T> const & mat2)
+bool verification (matrix_t<T> const & mat1 , const char  op  , matrix_t<T> const & mat2)
 {
 	matrix_t<T> result ;
 	bool success = true ;
